38665cf606fc4436d06e402f0190252dcf574d89
Vidya <vidyasritummalapalli@gmail.com>
2026-02-09 17:12:31 +0530

    modified frontend files

--- frontend/src/auth/authService.js
+++ frontend/src/auth/authService.js
@@ -4,3 +4,8 @@ export const login = async (formData) => {
   const res = await api.post("/auth/login", formData);
   return res.data;
 };
+
+export const registerTenant = async (formData) => {
+  const res = await api.post("/tenants/register", formData);
+  return res.data;
+};
