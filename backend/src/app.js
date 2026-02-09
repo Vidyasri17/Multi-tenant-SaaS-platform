078f14e6174b9057ff5b5e76a50e273306b24609
Vidya <vidyasritummalapalli@gmail.com>
2026-02-09 17:08:06 +0530

    modified app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,7 +17,7 @@ const app = express();
    GLOBAL MIDDLEWARES (FIRST)
    ============================ */
 app.use(cors({
-  origin: ["http://localhost:3000"],
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true
 }));
 
