38401645023874449d3c480be533dff4de142776
Vidya <vidyasritummalapalli@gmail.com>
2026-02-09 17:11:49 +0530

    modified frontend files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import PrivateRoute from "./routes/PrivateRoute.jsx";
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
       <Route
         path="/dashboard"
